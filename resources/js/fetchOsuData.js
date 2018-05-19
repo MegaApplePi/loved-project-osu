6003812f841c0956774d0dec1862967765450ca8
Eric X <123MegaApple123@gmail.com>
2018-05-19 14:16:15 -0500

    Allow skipping the osu!api fetch
    resolves #1

--- resources/js/fetchOsuData.js
+++ resources/js/fetchOsuData.js
@@ -10,11 +10,14 @@ let index;
 let newData;
 
 function noFetch() {
+  // add the beatmapset data to the original data (new data)
   newData.push(setData);
+  // send the new data to renderImages
   renderImages(newData);
 }
 
 function goFetch() {
+  // fetch stuff from the api
   fetch(`https://osu.ppy.sh/api/get_beatmaps?k=${key}&limit=1&s=${encodeURIComponent(ids[index])}`, {
     "cache": "no-cache",
     "credentials": "same-origin",
@@ -27,20 +30,31 @@ function goFetch() {
         "creator": response[0].creator,
         "title": response[0].title
       };
+      // is there any more to go though?
       if (index < ids.length - 1) {
+        // if so, fetch again
         goFetch(++index);
       } else {
+        // if not, we're done
         noFetch();
       }
     });
 }
 
 export default function fetchOsuData(data) {
+  // get the config
   config = getConfig();
   key = config.key;
 
-  newData = data;
-  ids = data[2];
-  index = 0;
-  goFetch();
+  // do we have the osu!api key?
+  if (key) {
+    // if so, go fetch
+    newData = data;
+    ids = data[2];
+    index = 0;
+    goFetch();
+  } else {
+    // if not, skip fetching
+    noFetch();
+  }
 }

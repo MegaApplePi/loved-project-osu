6ba51f457a38a98a6f72a9bf87f56ef096057f81
Eric X <123MegaApple123@gmail.com>
2018-05-21 01:15:49 -0500

    ignore .DS_Store file and more doc

--- resources/js/fetchIDs.js
+++ resources/js/fetchIDs.js
@@ -9,23 +9,30 @@ export default function fetchIDs(data) {
   newData = data;
   let files = data[1];
   let ids = [];
+  // go though the files
   for (let file of files) {
     if (
-      fs.existsSync(path.join(data[0], file)) &&
-      fs.statSync(path.join(data[0], file)).isFile() &&
-      !Number.isNaN(parseInt(file.split(".").shift(), 10)) &&
-      (/jpe?g|png/).test(file.split(".").pop())
+      // checking for PNG and JPG
+      fs.existsSync(path.join(data[0], file)) && // does it exist?
+      fs.statSync(path.join(data[0], file)).isFile() && // is it a file?
+      !Number.isNaN(parseInt(file.split(".").shift(), 10)) && // is it an actual id?
+      (/jpe?g|png/).test(file.split(".").pop()) // is the extension an image?
     ) {
+      // if so, add the id to the list
       ids.push(file.split(".").shift());
     } else if (
-      fs.existsSync(path.join(data[0], file)) &&
-      fs.statSync(path.join(data[0], file)).isFile() &&
-      file === "config.yaml"
+      // checking for config
+      fs.existsSync(path.join(data[0], file)) && // does it exist?
+      fs.statSync(path.join(data[0], file)).isFile() && // is it a file?
+      file === "config.yaml" // does the name match?
     ) {
+      // read it
       readConfig(path.join(data[0], file));
+      // report it
       notify(-1, "config file found");
     }
   }
   newData.push(ids);
+  // fetch data from the api
   fetchOsuData(newData);
 }
--- resources/js/window_drop.js
+++ resources/js/window_drop.js
@@ -1,30 +1,40 @@
 import {fs, path} from "./$$nodeRequire";
 import {$notify} from "./$$DOM";
 import fetchIDs from "./fetchIDs";
-// import renderImages from "./renderImages";
+import notify from "./notify";
 
 // drop event handler
 export default function window_drop(e) {
   // don't load the file
   e.preventDefault();
 
+  // clean up the notifications
   while ($notify.firstChild) {
     $notify.firstChild.remove();
   }
+  // get dropped folder
   let folderPath = e.dataTransfer.files[0].path;
 
+  // is the dropped folder really a folder?
   if (fs.statSync(folderPath).isDirectory()) {
+    // if so, read the contents
     let files = fs.readdirSync(folderPath);
 
+    // collect the files
     let onlyFiles = [];
     for (let file of files) {
       if (fs.statSync(path.join(folderPath, file)).isFile()) {
-        onlyFiles.push(file);
+        if (!(/^\.DS_Store$/i).test(file)) {
+          onlyFiles.push(file);
+        }
       }
     }
     let data = [folderPath, onlyFiles];
+    // let's get beatmap ids from the images
     fetchIDs(data);
-    // renderImages(folderPath, files);
+  } else {
+    // if not, error
+    notify(0, "Dropped folder is not a folder!");
   }
   return false;
 }
